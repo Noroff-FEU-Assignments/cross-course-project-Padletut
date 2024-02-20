608251d26252e519204efaa3da97c0e9a47f4327
Robert Nilsen <93103203+Padletut@users.noreply.github.com>
2024-02-20 21:30:00 +0100

    Refactor header.js, filter.js, renderCart.js, and detail.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,8 +1,5 @@
 import * as Constants from "./constants.js";
 import { loadFromStorage, saveToStorage } from "./storage/local.js";
-import { renderCarousel } from "./carousel.js";
-
-
 
 export async function fetchProducts(productContainer, loaderContainer) {
 
@@ -10,7 +7,7 @@ export async function fetchProducts(productContainer, loaderContainer) {
         let productStorage = loadFromStorage(Constants.productStorageKey) || { timestamp: 0, data: [] };
 
         // Check if data is in local storage and it's less than 1 minute old
-        if (productStorage.data.length > 0 && new Date().getTime() - productStorage.timestamp < 60000) {
+        if (productStorage.data.length !== 0 && (new Date().getTime() - productStorage.timestamp) <= 60000) {
             return productStorage.data;
         } else {
             const response = await fetch(Constants.url);
@@ -34,6 +31,7 @@ export async function fetchProducts(productContainer, loaderContainer) {
         if (loaderContainer) {
             loaderContainer.style.display = "none";
         }
+
         return productStorage.data;
     }
     catch (error) {
@@ -42,55 +40,12 @@ export async function fetchProducts(productContainer, loaderContainer) {
     }
 }
 
-export async function fetchProductsForCarousel(carouselContainer, loaderContainer, genderFilter = null) {
-    try {
-        let productStorage = loadFromStorage(Constants.productStorageKey) || { timestamp: 0, data: [] };
-
-        let data = productStorage.data;
-        // If data is not in local storage or it's more than 1 minute old, fetch it
-        if (data.length === 0 || new Date().getTime() - productStorage.timestamp >= 60000) {
-            const response = await fetch(Constants.url);
-            if (!response.ok) {
-                carouselContainer.innerHTML = '<div class="products__content__header"><h2>Ooops...something went wrong while loading the page</h2></div>';
-                return;
-            }
-
-            data = await response.json();
-            // Save data and timestamp to local storage
-            productStorage = { timestamp: new Date().getTime(), data };
-            saveToStorage(Constants.productStorageKey, productStorage);
-        }
-
-        // Check if loaderContainer exists before trying to access its properties
-        if (loaderContainer && loaderContainer.length > 0) {
-            loaderContainer[1].style.display = "none";
-        }
-
-        let filteredData = data;
-        if (genderFilter) {
-            filteredData = data.filter(product => product.attribute.terms[0].name === genderFilter);
-        }
-
-        const productList = filteredData.map(product => product.id);
-
-        // Initialize the carousel with a random set of 5 products
-        const randomProducts = filteredData.sort(() => 0.5 - Math.random()).slice(0, 5);
-        renderCarousel('.carousel__track', randomProducts);
-
-        saveToStorage(Constants.idKey, productList);
-        return filteredData;
-    }
-    catch (error) {
-        carouselContainer.innerHTML = '<div class="products__content__header"><h2>Ooops...something went wrong while loading the page</h2></div>';
-        console.warn(error);
-    }
-}
-
 export async function fetchSingleProduct(id, detailContainer, url, loaderContainer) {
     try {
         let productStorage = loadFromStorage(Constants.singleProductStorageKey) || { timestamp: 0, data: [] };
 
-        let data = productStorage.data.find(product => product.id === id);
+        let data = productStorage.data.find(product => product.id === Number(id));
+
         // If data is not in local storage or it's more than 1 minute old, fetch it
         if (!data || new Date().getTime() - productStorage.timestamp >= 60000) {
             const response = await fetch(`${url}/${id}`);
